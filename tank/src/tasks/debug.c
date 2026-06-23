8427613b74f06f335bb86d0b7a6c9fc3396ca445
NeLaurynas <ndisposable@gmail.com>
2026-06-23 12:55:10 +0300

    debug

--- tank/src/tasks/debug.c
+++ tank/src/tasks/debug.c
@@ -3,6 +3,7 @@
 
 #include <FreeRTOS.h>
 #include <task.h>
+#include <utils.h>
 
 #include "shared_modules/cpu_cores/cpu_cores.h"
 #include "state.h"
@@ -15,7 +16,7 @@ void task_debug(void *task_parameter) {
 	state.tasks.shutdown.last_wake = 0;
 	tasks_delay(&state.tasks.shutdown);
 
-	// cpu_cores_shutdown_from_core0(); // never returns
+	utils_error_mode(36);
 
-	vTaskDelete(nullptr);
+	// cpu_cores_shutdown_from_core0(); // never returns
 }
